6cb266728de6a063a29cb3b432cab48645dd3891
devbewill <sedprc@gmail.com>
2021-12-15 18:12:56 +0100

    born

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import RulerSVG from "./assets/ruler.svg";
 import { ReactComponent as BabySVG } from "./assets/baby.svg";
 import CountBabyAge from "./components/CountBabyAge";
 import CountdownTimer from "./components/CountdownTimer";
+import Born from "./components/Born";
 
 function App() {
   return (
     <>
-    <div className="logo">toctocinviaarona14</div>
-    <div className="container">
-      <div className="circle"></div>
-      <BabySVG className="baby" />
-      <div className="ruler">
-        <p>dimensioni attuali </p>
-        <img src={RulerSVG} alt="ruler" />
-      </div>
+      <div className="logo">toctocinviaarona14</div>
+      <div className="container">
+        <div className="circle"></div>
+        <BabySVG className="baby  " />
+        <div className="ruler">
+          {/* <p>3,1 kg </p> */}
+          {/* <img src={RulerSVG} alt="ruler" /> */}
+        </div>
+        <Born></Born>
 
-      <CountBabyAge />
-      <CountdownTimer />
-    </div>
+        {/* <CountBabyAge />
+      <CountdownTimer /> */}
+      </div>
     </>
   );
 }
---
+++ src/components/Born.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+const Born = () => {
+  return (
+    <>
+      <div className="born-box">
+        <h2>Cecilia è arrivata!</h2>
+
+        <p>
+          Alla fine ha optato per il 12 Dicembre 2021, alle ore 12:28 ha fatto
+          il suo primo strillo e pare le sia piaciuto molto 😭
+        </p>
+        <p>Comunque sia, 3.1kg di amore puro 😍</p>
+      </div>
+    </>
+  );
+};
+
+export default Born;
